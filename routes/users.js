1049e7beab01957b5b94f4a3c65984027b983b23
mariia <mariia.kondak@nure.ua>
2024-01-24 03:36:57 +0100

    Added users collection. Implemented login logic

---
+++ routes/users.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const userRoutes = express.Router();
+const dbo = require("../db/conn");;
+
+userRoutes.route("/users/:email").get(async function (req, res) {
+  const db_connect = dbo.getDb("fast_burger");
+  const userEmail = req.params.email;
+
+  try {
+    const user = await db_connect
+      .collection("users")
+      .findOne({ email: userEmail });
+
+    if (user) {
+      const { _id, name, email, role, address, telephone } = user;
+      res.json({ _id, name, email, role, address, telephone });
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (err) {
+    console.error("Error fetching user:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+userRoutes.route("/login").post(async function (req, res) {
+  const db_connect = dbo.getDb("fast_burger");
+  const { email, hashedPassword } = req.body;
+
+  try {
+    const user = await db_connect.collection("users").findOne({ email });
+
+    if (user && user.password === hashedPassword) {
+      const { _id, name, email, role, address, telephone } = user;
+      res.json({ _id, name, email, role, address, telephone });
+    } else {
+      res.status(401).json({ error: "Invalid credentials" });
+    }
+  } catch (err) {
+    console.error("Error fetching user:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+module.exports = userRoutes;
